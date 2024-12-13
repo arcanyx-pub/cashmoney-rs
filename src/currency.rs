51a876e9fc112ee742eb50bbce1c0539b6f68791
Joe Dahlquist <joe@arcanyx.com>
2024-12-13 12:34:58 -0800

    remove ye olde crufte

--- src/currency.rs
+++ src/currency.rs
@@ -1,5 +1,3 @@
-use std::str::FromStr;
-
 /// Supported currencies, identified by their ISO 4217 code.
 #[derive(Copy, Clone, Debug, PartialEq)]
 pub enum Currency {
@@ -9,26 +7,11 @@ pub enum Currency {
     CAD,
 }
 
-pub struct UnknownCurrencyError {}
-
 impl Currency {
     pub fn max_precision(&self) -> u32 {
         match self {
             Currency::USD => 2,
             Currency::CAD => 2,
-        }
-    }
-}
-
-impl FromStr for Currency {
-    type Err = UnknownCurrencyError;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let s = s.to_ascii_uppercase();
-        match s.as_ref() {
-            "USD" => Ok(Currency::USD),
-            "CAD" => Ok(Currency::CAD),
-            _ => Err(UnknownCurrencyError {}),
         }
     }
 }
